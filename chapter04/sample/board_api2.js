0c543923aede800551bb934d78b9b0415cbf879d
MinkyungPark <parkminkyung32@gmail.com>
2021-02-19 02:34:10 +0900

    chapter04

--- chapter04/sample/board_api2.js
+++ chapter04/sample/board_api2.js
@@ -1,4 +1,6 @@
 const morgan = require('morgan');
+const url = require('url');
+const uuidAPIkey = require('uuid-apikey');
 
 /* express app generate */
 const express = require('express');
@@ -11,6 +13,12 @@ app.set('port', process.env.PORT || 8080);
 app.use(morgan('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+
+/* 테스트를 위한 API키 */
+const key = {
+    apiKey: '3CCHAAM-C0ZMC86-QB498ZA-QRDE88V',
+    uuid: '1b19152a-603f-4620-bac8-947dbe1ae423'
+};
 
 /* 테스트를 위한 게시글 데이터 */
 let boardList = [];
@@ -72,9 +80,35 @@ app.delete('/board/:id', (req, res) => {
     res.redirect('/board');
 });
 
+/* 게시글 검색 API using uuid-key */
+app.get('/board/:apikey/:type', (req, res) => {
+    let { type, apikey } = req.params;
+    const queryData = url.parse(req.url, true).query;
+
+    if (uuidAPIkey.isAPIKey(apikey) && uuidAPIkey.check(apikey, key.uuid)) {
+        if (type === 'search') { // 키워드로 게시글 검색  
+            const keyword = queryData.keyword;
+            const result = boardList.filter((e) => {
+                return e.title.includes(keyword)
+            })
+            res.send(result);
+        }
+        else if (type === 'user') { // 닉네임으로 게시글 검색  
+            const user_id = queryData.user_id;
+            const result = boardList.filter((e) => {
+                return e.user_id === user_id;
+            });
+            res.send(result);
+        }
+        else {
+            res.send('Wrong URL')
+        }
+    } else {
+        res.send('Wrong API Key');
+    }
+});
+
 /* 서버와 포트 연결.. */
 app.listen(app.get('port'), () => {
     console.log(app.get('port'), '번 포트에서 서버 실행 중 ..')
 });
-
-//localhost:8080/board/3CCHAAM-C0ZMC86-QB498ZA-QRDE88V/search?keyword=호호
