1a97503161f065d97c04877c4f96d0a9dfd472fb
박민경 <parkminkyung32@gmail.com>
2022-03-03 10:29:19 +0900

    index.css 적용되지 않는 문제 수정

--- chapter06/chat/app.js
+++ chapter06/chat/app.js
@@ -2,6 +2,8 @@ const http = require("http");
 const express = require("express");
 const app = express();
 
+app.use(express.static(__dirname)); 
+
 const server = http.Server(app);
 const io = require("socket.io")(server);
 let users = [];
