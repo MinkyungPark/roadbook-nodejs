2a085c9f0387f1dc360b780d8e77c7e4fd3c77e1
박민경 <parkminkyung32@gmail.com>
2022-04-29 15:16:00 +0900

    redis v4이상 용 코드 추가

--- chapter04/sample/redis.js
+++ chapter04/sample/redis.js
@@ -1,11 +1,24 @@
-/* redis version 3.1.2 */
-/* npm install redis@^3.1.2 */
-
 const redis = require('redis');
 const client = redis.createClient(6379, '127.0.0.1');
+
+/* redis version 3.1.2 */
+/* npm install redis@^3.1.2 */
 
 client.get('myKey', (err, value) => {
     console.log(value);
 });
 
 
+/* if redis version over 4 */
+//async function run(){
+//     await client.connect()
+// }
+
+// run();
+
+// async function getVal(key){
+//     const item = await client.lRange(key, 0, -1);
+//     console.log(item);
+// }
+
+// getVal("myKey");
